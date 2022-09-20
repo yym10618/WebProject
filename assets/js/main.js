95bf102b6a1b317a4e1bece0c758bb1c1c287877
unknown <yym10618@gmail.com>
2022-09-20 21:48:23 +0900

    intro

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ import { bs } from './modules/bsComponents.js'
 import {shadow} from './modules/shadow.js'
 import {wowAnimate} from './modules/wowAnimate.js'
 import {scroll} from './modules/scroll.js'
+import {naverMap} from './modules/naverMap.js'
 
 bs()
 if(document.querySelector('#shadow-box')){
@@ -14,4 +15,8 @@ if(document.querySelector('.wow')){
 
 if(document.querySelector('#scrolltoTop')){
     scroll()
+}
+
+if(document.querySelector('#map')){
+    naverMap()
 }
