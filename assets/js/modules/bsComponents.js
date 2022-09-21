f3895b2d6df62d5a4349ca325c6464e2d1c9b679
unknown <yym10618@gmail.com>
2022-09-21 20:19:49 +0900

    post

--- assets/js/modules/bsComponents.js
+++ assets/js/modules/bsComponents.js
@@ -4,4 +4,8 @@ export function bs(){
     var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl){
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
+
+    // Bootstrap 5 popover
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 }
